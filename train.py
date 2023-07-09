477f98f9cca5fcdef91f539f20db94e6842b7f1c
Valentin <valentinvial42.vv@gmail.com>
2023-07-09 22:07:41 +0200

    added fine_tuning method

--- train.py
+++ train.py
@@ -1,7 +1,19 @@
 from ultralytics import YOLO
 import yaml
 
-if __name__ == "__main__":
+def fine_tunining(path_to_last_weight,additionnal_epochs) :
+    model = YOLO(path_to_last_weight)
+    model.resume = True 
+
+    # train the model
+    results = model.train(
+        epochs=additionnal_epochs, # number of additional epochs you want to train on
+        imgsz=640,
+        lr0=0.01,  # initial learning rate (i.e. SGD:1E-2, Adam:1E-3)
+        lrf=0.01
+    )
+
+def start_training_scratch() :
     params_path = "./configs/nano/params.yaml"
 
     #Loading model config
@@ -21,3 +33,8 @@ if __name__ == "__main__":
     print(">> Starting training <<")
     print(str(args))
     results = model.train(**args)
+
+if __name__ == "__main__":
+    path_to_last_weight = ""
+    additionnal_epochs = 10
+    fine_tunining(path_to_last_weight,additionnal_epochs)

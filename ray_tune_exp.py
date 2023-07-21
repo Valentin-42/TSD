df0c78f00e627f52db0240f5b14655c88f7aaeab
Valentin <valentinvial42.vv@gmail.com>
2023-07-21 13:30:39 +0200

    vdfsdf

--- ray_tune_exp.py
+++ ray_tune_exp.py
@@ -25,7 +25,8 @@ if __name__ == "__main__":
                             space=param_space,
                             gpu_per_trial=1,
                             max_samples= 5,
-                            epochs=50)
+                            epochs=50,
+                            batch=32)
     
     print("   >>>>>    ")
     print(result_grid)

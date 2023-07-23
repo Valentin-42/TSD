fc606968187ebf2f1f5d35e779ab7c25f1112e61
Valentin <valentinvial42.vv@gmail.com>
2023-07-23 11:57:26 +0200

    ilsqhc

--- val.py
+++ val.py
@@ -7,6 +7,6 @@ model = YOLO(model_weights)  # load a custom model
 
 print("model loaded")
 # Validate the model
-metrics = model.val(data="./configs/nano_local/data_1class.yaml",save_json=True)  # no arguments needed, dataset and settings remembered
+metrics = model.val(save_json=True)  # no arguments needed, dataset and settings remembered
 
 print(f"{metrics.box.maps}")
